2afc907a0b023f5322d41ef475d0b7b90ee6bc1b
Freddy Rioboo <rioboo.2@hotmail.com>
2022-09-30 12:07:58 -0600

    API's functionalities created and tested

--- Aplicaciones/Academico/urls.py
+++ Aplicaciones/Academico/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from Aplicaciones.Academico.views import CursoListView
-from .views  import actualizar_curso, eliminar_curso, registrar_curso, edicion_curso, contacto
+from .views  import *
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('eliminacionCurso/<int:id>', eliminar_curso),
     path('edicionCurso/<int:id>', edicion_curso),
     path('actualizarCurso/', actualizar_curso),
-    path('contacto', contacto)
+    path('contacto', contacto),
+    path('cursos/', CursoApi.as_view(), name= 'api_cursos'),
+    path('cursos/<int:id>', CursoApi.as_view(), name= 'api_curso'),
 ]  
--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,6 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from pkg_resources import cleanup_resources
 from .models import Curso
 from django.views.generic import ListView
+from django.views import View
+import json
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+
 
 
 # Create your views here.
@@ -51,3 +60,82 @@ def actualizar_curso(request):
 
 def contacto(request):
     return render(request, 'contacto.html')
+
+
+class CursoApi(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if id > 0:
+            cursos = list(Curso.objects.filter(id=id).values())
+            if len(cursos) > 0:
+                curso = cursos[0]
+                data = {
+                    'message': 'Success', 'curso': curso
+                }
+                return JsonResponse(data)
+            else:
+                data = {
+                    'message': 'Curso not Found'
+                }
+                return JsonResponse(data)
+        else: 
+            cursos = list(Curso.objects.values())
+            if len(cursos) > 0:
+                data ={
+                    'message': 'Success', 'curso': cursos
+                }
+                return JsonResponse(data)
+            else:
+                data = {
+                    'message': 'Curso not Found'
+                }
+                return JsonResponse(data)
+    
+
+    def post(self, request):
+        jd = json.loads(request.body)
+        print(jd)
+        Curso.objects.create(nombre=jd['nombre'], creditos=jd['creditos'])
+        data = {
+                'message': 'Success'
+                }
+        return JsonResponse(data)
+
+    
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        cursos = list(Curso.objects.filter(id=id).values())
+        if len(cursos) > 0:
+            curso = Curso.objects.get(id=id)
+            curso.nombre = jd['nombre']
+            curso.creditos = jd['creditos']
+            curso.docente_id = jd['docente_id']
+            curso.save()
+            data = {
+                'message': 'Success'
+                }
+        else: 
+            data = {
+                    'message': 'Curso not Found'
+                }
+        return JsonResponse(data)
+
+    def delete(self, request, id):
+        curso = list(Curso.objects.filter(id=id).values())
+        if len(curso) > 0:
+            Curso.objects.filter(id=id).delete()
+            data = {
+                'message': 'Success'
+                }
+        else: 
+            data = {
+                    'message': 'Curso not Found'
+                }
+        return JsonResponse(data)
+    
+
+
